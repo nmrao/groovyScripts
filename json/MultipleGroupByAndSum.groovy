d980f7c6d479eae43d3b4983d629ac12de426718
nmrao <nagamalli.s@gmail.com>
2017-05-18 00:10:34 +0530

    Create MultipleGroupByAndSum.groovy

---
+++ json/MultipleGroupByAndSum.groovy
@@ -0,0 +1,34 @@
+/**
+* Refer: http://stackoverflow.com/questions/44026268/group-by-several-parameters-in-groovy-sql-like
+**/
+def str = """[
+	{
+		"Country": "Norway",
+		"Dept": "2",
+		"Hours": 08.00
+	},
+	{
+		"Country": "Norway",
+		"Dept": "2",
+		"Hours": 08.00
+	},
+	{
+		"Country": "Sweden",
+		"Dept": "1",
+		"Hours": 08.00
+	},
+	{
+		"Country": "Sweden",
+		"Dept": "2",
+		"Hours": 08.00
+	}
+]"""
+ 
+def objJson = new groovy.json.JsonSlurper().parseText(str)
+ 
+def list = objJson.groupBy({it.Country}, {it.Dept})
+                  .collectEntries { k, v -> [k, v.collect{ [Hours: it.value.Hours.sum(), Dept: it.value.Dept[0], Country: it.value.Country[0]] }]}
+                  .collect{ k, v -> v }
+                  .flatten()
+ 
+print new groovy.json.JsonBuilder(list).toPrettyString()
